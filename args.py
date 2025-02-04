52cd099ce650c7055f2c30c2898e8e0f69377456
cgao-comp <81541997+cgao-comp@users.noreply.github.com>
2025-02-04 12:23:59 +0800

    Add files via upload

---
+++ args.py
@@ -0,0 +1,15 @@
+platform1 = 'Twitter'
+platform2 = 'Weibo'
+platform3 = 'Facebook'
+platform4 = '/syn'
+device = 'cuda'
+
+epochs = 60
+
+beta_1 = 0.0001
+beta_T = 0.01
+T = 100
+
+
+scale = 0.02
+
